a36b21da0e5bc1449970cb8eb91191d6ecf8b5f7
Uwe Franke <uwe.franke@outlook.com>
2026-04-23 20:29:08 +0200

    fix: KRITISCH - Doppelte Funktionsdeklaration entfernt
    FATAL ERROR behoben:
    - potsdam_get_service_icon() war zweimal deklariert
    - Zeile 11: Alte Version (verkehrsrecht, familienrecht, etc.)
    - Zeile 91: Neue Version (scale, gavel, file-text, etc.)
    
    FIX:
    - Alte Funktion (Zeile 8-24) komplett entfernt
    - Nur neue rechtsspezifische Icon-Funktion bleibt erhalten
    
    Website funktioniert jetzt wieder!

--- src/inc/template-tags.php
+++ src/inc/template-tags.php
@@ -6,23 +6,6 @@
  */
 
 /**
- * Service-Icon ausgeben
- */
-function potsdam_get_service_icon($type) {
-    $icons = array(
-        'verkehrsrecht' => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="1" y="3" width="15" height="13"></rect><polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon><circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle></svg>',
-        
-        'familienrecht' => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>',
-        
-        'vertragsrecht' => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line></svg>',
-        
-        'immobilienrecht' => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>',
-    );
-    
-    return isset($icons[$type]) ? $icons[$type] : '';
-}
-
-/**
  * Formatierte Adresse ausgeben
  */
 function potsdam_get_formatted_address() {
